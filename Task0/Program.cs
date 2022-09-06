986d4bcd08ec94cfe813fce3a3f470ba0bec5465
Mikitas <ymikitas@gmail.com>
2022-09-06 11:03:31 +0300

    Add Task 1-8

---
+++ Task0/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт его квадрат.
+
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt16(Console.ReadLine());
+// int sqr = number * number;
+// System.Console.WriteLine($"Квадрат числа {number} равен {sqr}");
+int sqr = Convert.ToInt32(Math.Pow(number, 2)); // Втроенный метод
+System.Console.WriteLine($"Квадрат числа {number} равен {sqr}");
